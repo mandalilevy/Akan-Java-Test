ea76142a09f9fb710aa01777a35596aab6594409
Levy Mandali <mandalilevy@gmail.com>
2021-11-19 13:16:14 +0300

    first commit

---
+++ src/akan/Akan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akan;
+
+/**
+ *
+ * @author LevyMandali
+ */
+public class Akan {
+public static int mymethod(){
+    return 5*2;
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//     int i=8;
+//     System.out.println(i++);
+//     System.out.println(i++);
+//     System.out.println(++i);
+System.out.println(mymethod());
+    }
+    
+}
